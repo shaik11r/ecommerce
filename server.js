c25391de1da93f612ea5aa30e79e4d9cd5a17ac2
shaik11r <nadeencool11@gmail.com>
2022-06-20 15:40:00 +0530

    created server and dbconfig file

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
- 
+ const expresss =require('express');
+ const serverConfig=require('./configs/server.config');
+ const app=expresss();
+ app.listen(serverConfig.PORT,()=>{
+    console.log(`APPLICATION STARTED on port no: ${serverConfig.PORT}`);
+ }) 
